87b5c1feabd40a4978a3a35fe2a63abca87fc543
Madison Grace Thantu <madisonthantu@gmail.com>
2022-11-13 11:47:03 -0500

    First commit

---
+++ src/main/java/com/example/maven_auth0_demo/MavenAuth0DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven_auth0_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAuth0DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MavenAuth0DemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/maven_auth0_demo/MavenAuth0DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven_auth0_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenAuth0DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
